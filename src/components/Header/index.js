8e09ce1c501670c0fd740296bb604aaa426c2ee7
kylest0kes <kylestokes1551@gmail.com>
2020-09-15 21:15:09 -0400

    iit react app, linked Foundation, and started header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div class="top-bar topbar-center-logo" id="topbar-center-logo">
+            <div class="top-bar-left">
+                <ul class="menu vertical medium-horizontal">
+                    <li><a href="#">One</a></li>
+                    <li><a href="#">Two</a></li>
+                    <li><a href="#">Three</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
