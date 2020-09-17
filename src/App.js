a91544f94ec5a53ce1025c2cac5825ec00395881
kylest0kes <kylestokes1551@gmail.com>
2020-09-17 19:12:14 -0400

    created footer component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Projects />
       <Resume />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './style.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>Kyle Stokes</li>
+                <li>©2020</li>
+            </ul>
+        </footer>
+    )
+}
