ffff778ba3a150c134f4501c53198576ec10c5f9
kylest0kes <kylestokes1551@gmail.com>
2020-09-24 21:36:39 -0400

    set up layout beginning of the about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,24 @@ export default function About() {
         <div className="jumbotron">
             <h1>About</h1>
             <hr/>
+            <div className="row about-three">
+                <div className="col">
+                    <p><i className="fas icon-three fa-thumbs-up"></i></p>
+                    <h4 className="marketing-site-three-up-title">Keyless Entry</h4>
+                    <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
+                </div>
+                <div className="col">
+                    <p><i className="fa icon-three fa-user-o" aria-hidden="true"></i></p>
+                    <h4 className="marketing-site-three-up-title">Invincible Igloo</h4>
+                    <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
+                </div>
+                <div className="col">
+                    <p><i className="fa icon-three fa-user-o" aria-hidden="true"></i></p>
+                    <h4 className="marketing-site-three-up-title">Invincible Igloo</h4>
+                    <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,33 +4,35 @@ import './style.css'
 
 export default function Contact() {
     return (
-        <div className="jumbotron contact">
-            <h1>Contact</h1>
-            <hr/>
-            <h4>For business inquires, questions, or comments</h4>
-            <form action="">
-                <div className="row">
-                    <div className="small-12 column">
-                        <div className="form-floating-label">
-                            <input type="text" id="name" name="name" required/>
-                            <label for="name">Name</label>
+        <div className="container">
+            <div className="jumbotron contact">
+                <h1>Contact</h1>
+                <hr/>
+                <h4>For business inquires, questions, or comments</h4>
+                <form action="">
+                    <div className="row">
+                        <div className="small-12 column">
+                            <div className="form-floating-label">
+                                <input type="text" id="name" name="name" required/>
+                                <label for="name">Name</label>
+                            </div>
+                        </div>
+                        <div className="small-12 column">
+                            <div className="form-floating-label">
+                                <input type="text" id="email" name="email"/>
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+                        <div className="small-12 column">
+                            <div className="form-floating-label">
+                                <textarea name="" id="" rows="5" placeholder=""></textarea>
+                                <label for="textarea">Your Message</label>
+                            </div>
                         </div>
                     </div>
-                    <div className="small-12 column">
-                        <div className="form-floating-label">
-                            <input type="text" id="email" name="email"/>
-                            <label for="email">Email</label>
-                        </div>
-                    </div>
-                    <div className="small-12 column">
-                        <div className="form-floating-label">
-                            <textarea name="" id="" rows="5" placeholder=""></textarea>
-                            <label for="textarea">Your Message</label>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <button type="submit" class="btn">Send</button>
-        </div>
+                </form>
+                <button type="submit" class="btn">Send</button>
+            </div>
+        </div>    
     )
 }
