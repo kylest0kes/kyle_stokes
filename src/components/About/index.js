757c8134f45eccb38810672a8f11fa1c3e200b0f
kylest0kes <kylestokes1551@gmail.com>
2020-09-24 21:43:27 -0400

    set up about me section with foundation grid instead of bootstrap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,23 +7,26 @@ export default function About() {
         <div className="jumbotron">
             <h1>About</h1>
             <hr/>
-            <div className="row about-three">
-                <div className="col">
-                    <p><i className="fas icon-three fa-thumbs-up"></i></p>
-                    <h4 className="marketing-site-three-up-title">Keyless Entry</h4>
-                    <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
-                </div>
-                <div className="col">
-                    <p><i className="fa icon-three fa-user-o" aria-hidden="true"></i></p>
-                    <h4 className="marketing-site-three-up-title">Invincible Igloo</h4>
-                    <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
-                </div>
-                <div className="col">
-                    <p><i className="fa icon-three fa-user-o" aria-hidden="true"></i></p>
-                    <h4 className="marketing-site-three-up-title">Invincible Igloo</h4>
-                    <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
+            <div class="grid-container">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-4">
+                        <p><i className="fas icon-three fa-thumbs-up"></i></p>
+                        <h4 className="marketing-site-three-up-title">Keyless Entry</h4>
+                        <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p> 
+                    </div>
+                    <div class="cell small-4">
+                        <p><i className="fas icon-three fa-thumbs-up"></i></p>
+                        <h4 className="marketing-site-three-up-title">Keyless Entry</h4>
+                        <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
+                    </div>
+                    <div class="cell small-4">
+                        <p><i className="fas icon-three fa-thumbs-up"></i></p>
+                        <h4 className="marketing-site-three-up-title">Keyless Entry</h4>
+                        <p className="marketing-site-three-up-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi sint, voluptatibus eaque natus ad eius.</p>
+                    </div>
                 </div>
             </div>
+            
             
         </div>
     )
