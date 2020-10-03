c53c60785c8a6981ae88515457ad739ceb99bf4e
kylest0kes <kylestokes1551@gmail.com>
2020-10-02 20:35:23 -0400

    added text links to socials for view

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,6 +9,17 @@ export default function Contact() {
                 <h1>Contact</h1>
                 <hr/>
                 <h4>For business inquires, questions, or comments</h4>
+                <ul className="stats-list">
+                    <li>
+                        LinkedIn <span className="stats-list-label">linkedin.com/in/kylest0kes/</span>
+                    </li>
+                    <li>
+                        Email <span className="stats-list-label">kylestokes1551@gmail.com</span>
+                    </li>
+                    <li>
+                        Github <span className="stats-list-label">github.com/kylest0kes</span>
+                    </li>
+                </ul>
                 <form action="">
                     <div className="row">
                         <div className="small-12 column">
